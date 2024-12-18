7be51c97203d129a1549f72293beb78d154f30f9
Vadim Kulibaba <recoilme@users.noreply.github.com>
2024-12-18 12:45:32 +0300

    576

--- train_scripts/downscale.py
+++ train_scripts/downscale.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 def downscale_image_by(image, max_size,x=64):
     try:
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
         image = np.array(image)
         height, width = image.shape[:2]
         if width > height:
@@ -12,12 +14,14 @@ def downscale_image_by(image, max_size,x=64):
         else:
             new_height = max_size
             new_width = int(width * (max_size / height))
+        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
         new_width = (new_width // x) * x
         new_height = (new_height // x) * x
-        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+        image = image[:new_height, :new_width]
+        height, width = image.shape[:2]
+        if height!=new_height or width!=new_width:
+            image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
         image = Image.fromarray(image)
-        if image.mode == "RGBA":
-            image = image.convert("RGB")
         return image
     except Exception as e:
         print(f"Error downscaling image: {e}")
--- train_scripts/train_waifu.py
+++ train_scripts/train_waifu.py
@@ -90,7 +90,7 @@ def idation(accelerator, config, model, step, device, vae=None, init_noise=None)
         for prompt in validation_prompts:
             logger.info(prompt)
             z = (
-                torch.randn(1, config.vae.vae_latent_dim, latent_size, latent_size, device=device)
+                torch.randn(1, config.vae.vae_latent_dim, int(latent_size*1.25),int(latent_size//1.125), device=device)
                 if init_z is None
                 else init_z
             )
@@ -163,9 +163,9 @@ def idation(accelerator, config, model, step, device, vae=None, init_noise=None)
     image_logs += run_sampling(init_z=None, label_suffix="", sampler=vis_sampler)
 
     # Second run with init_noise if provided
-    if init_noise is not None:
-        init_noise = torch.clone(init_noise).to(device)
-        image_logs += run_sampling(init_z=init_noise, label_suffix=" w/ init noise", sampler=vis_sampler)
+    #if init_noise is not None:
+    #    init_noise = torch.clone(init_noise).to(device)
+    #    image_logs += run_sampling(init_z=init_noise, label_suffix=" w/ init noise", sampler=vis_sampler)
 
     if del_vae:
         vae = None
@@ -645,7 +645,7 @@ def main(cfg: SanaConfig) -> None:
     max_length = config.text_encoder.model_max_length
     vae = None
     validation_noise = (
-        torch.randn(1, config.vae.vae_latent_dim, latent_size, latent_size, device="cpu", generator=generator)
+        torch.randn(1, config.vae.vae_latent_dim, int(latent_size*1.25),int(latent_size//1.125), device="cpu", generator=generator)
         if getattr(config.train, "deterministic_validation", False)
         else None
     )
